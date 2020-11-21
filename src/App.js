88bffd8b3bdd374d62f5e2d779420c867abc7e24
Brandon Fonnemann <branjosef@gmail.com>
2020-11-21 00:26:45 -0600

    commit tree 65e3dc5c82feb610c9863c2486d19b1b07ed16f8

--- src/App.js
+++ src/App.js
@@ -1,1 +1,32 @@
-// create your App component here
+import React from 'react'
+
+
+
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      astronauts: []
+    }
+  }
+
+
+    componentDidMount(){
+      fetch('http://api.open-notify.org/astros.json')
+      .then(resp => resp.json())
+      .then(astros => {
+          this.setState({
+            astronauts: astros.people
+          })
+      })
+    }
+
+  render(){
+    return(
+      <div>{this.state.astronauts.map(person => person.name)}</div>
+    )
+  }
+
+}
+
+export default App
